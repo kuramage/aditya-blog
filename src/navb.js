5dab4021e177c0bd8b86ab9d804060c20ad8fa0e
Aditya Singh Yadav <aditya22039@iiitd.ac.in>
2024-09-01 23:17:16 +0530

    creating Textutils application using bootstrap Css and React JS

---
+++ src/navb.js
@@ -0,0 +1,12 @@
+const navb = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="links">
+                <a href='/'>home</a>
+                <a href='/create'>New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default navb;
