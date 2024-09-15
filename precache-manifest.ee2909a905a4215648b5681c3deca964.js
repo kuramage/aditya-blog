d32f0ada740ec7fe0996d1e05b7cb8d858305c78
Aditya Singh Yadav <aditya22039@iiitd.ac.in>
2024-09-15 22:54:46 +0530

    Updates

---
+++ precache-manifest.ee2909a905a4215648b5681c3deca964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784b824c35bd544938c2c7fb46209ae5",
+    "url": "/aditya-blog/index.html"
+  },
+  {
+    "revision": "8fbf38686cd8b9f81333",
+    "url": "/aditya-blog/static/css/main.16657954.chunk.css"
+  },
+  {
+    "revision": "a2c5c3ac168b7075fbcb",
+    "url": "/aditya-blog/static/js/2.a58ae963.chunk.js"
+  },
+  {
+    "revision": "8fbf38686cd8b9f81333",
+    "url": "/aditya-blog/static/js/main.5479e5fa.chunk.js"
+  },
+  {
+    "revision": "964f0c714c192805e51f",
+    "url": "/aditya-blog/static/js/runtime~main.a3e2fec5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba9a6073ce782d1cf6419479b0685f80.js"
+  "/aditya-blog/precache-manifest.ee2909a905a4215648b5681c3deca964.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aditya-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
